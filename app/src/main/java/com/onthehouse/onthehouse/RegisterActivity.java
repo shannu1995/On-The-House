32227b19091275ac695a1c09378e8b88d6c7b6b0
Abu Bakar Khan <s3597217@student.rmit.edu.au>
2017-08-24 00:54:25 +1000

    Register

--- app/src/main/java/com/onthehouse/onthehouse/RegisterActivity.java
+++ app/src/main/java/com/onthehouse/onthehouse/RegisterActivity.java
@@ -1,11 +1,9 @@
 package com.onthehouse.onthehouse;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,17 +14,13 @@ public class RegisterActivity extends AppCompatActivity {
 
         final EditText email = (EditText) findViewById(R.id.emailRegister);
         final EditText password = (EditText) findViewById(R.id.passwordRegister);
-        final EditText passwordConfirmation = (EditText) findViewById(R.id.passwordConfirmation);
         final EditText firstName = (EditText) findViewById(R.id.firstName);
         final EditText lastName = (EditText) findViewById(R.id.lastName);
         final EditText nickName = (EditText) findViewById(R.id.nickName);
 
-        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.states_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
-        final Button register = (Button) findViewById(R.id.bRegister);
     }
 }
