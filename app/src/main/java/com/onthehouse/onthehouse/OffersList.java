2eb479a40305d940698dbc1bf07c3d47bd361c2c
shannu1995 <s3453738@student.rmit.edu.au>
2017-08-23 15:04:53 +1000

    Added text fields and white coloring

--- app/src/main/java/com/onthehouse/onthehouse/OffersList.java
+++ app/src/main/java/com/onthehouse/onthehouse/OffersList.java
@@ -3,11 +3,11 @@ package com.onthehouse.onthehouse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OffersList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_offers_list);
     }
 }
