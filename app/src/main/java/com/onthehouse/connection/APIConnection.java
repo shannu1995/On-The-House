440a3afc036914b07832ec3d196684b07d194fc6
haseebjehangir <haseebjehangir@gmail.com>
2017-08-24 02:26:26 +1000

    post call

---
+++ app/src/main/java/com/onthehouse/connection/APIConnection.java
@@ -0,0 +1,104 @@
+package com.onthehouse.connection;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class APIConnection
+{
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	// HTTP GET request
+	public void sendGet() throws Exception {
+
+		String url = "http://ma.on-the-house.org/api/v1/zones/13";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null)
+        {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	public StringBuffer sendPost(String urlCall, ArrayList<String> parameterList) throws Exception
+    {
+        StringBuffer response = new StringBuffer();
+
+		try
+		{
+            // open a connection to the site
+            URL url = new URL("http://ma2.on-the-house.org"+urlCall);
+            URLConnection con = url.openConnection();
+            // activate the output
+            con.setDoOutput(true);
+            PrintStream ps = new PrintStream(con.getOutputStream());
+
+            for(int i=0; i<parameterList.size(); i++)
+            {
+                ps.print("&"+parameterList.get(i));
+            }
+            // send your parameters to your site
+            //ps.print("&email=abubakar128@hotmail.com");
+            //ps.print("&password=qwerty5432");
+
+            // we have to get the input stream in order to actually send the request
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    		String inputLine;
+
+    		while ((inputLine = in.readLine()) != null)
+    		{
+    			response.append(inputLine);
+    		}
+    		in.close();
+
+    		System.out.println(response);
+            // close the print stream
+            ps.close();
+        }
+		catch (MalformedURLException e1)
+		{
+                e1.printStackTrace();
+        }
+		catch (IOException e2)
+		{
+                e2.printStackTrace();
+        }
+
+        return response;
+	}
+
+}
--- app/src/main/java/com/onthehouse/onthehouse/LoginActivity.java
+++ app/src/main/java/com/onthehouse/onthehouse/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.onthehouse.onthehouse;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,72 +11,76 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.onthehouse.connection.APIConnection;
+
 import java.util.ArrayList;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            if (android.os.Build.VERSION.SDK_INT > 9)
-            {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }
-
-            final EditText email = (EditText) findViewById(R.id.loginEmail);
-            final EditText password = (EditText) findViewById(R.id.loginPassword);
-            final Button loginButton= (Button)findViewById(R.id.loginButton);
-
-            final TextView signUp = (TextView) findViewById(R.id.signup);
-            final TextView skip = (TextView) findViewById(R.id.skip);
-            final TextView reset = (TextView) findViewById(R.id.resetPassword);
-
-
-            loginButton.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    String emailStr = email.getText().toString();
-                    String passStr = password.getText().toString();
-
-                    APIConnection connection = new APIConnection();
-                    ArrayList<String> inputList = new ArrayList<String>();
-                    inputList.add("email="+emailStr);
-                    inputList.add("password="+passStr);
-                    try
-                    {
-                        StringBuffer output = connection.sendPost("/api/v1/member/login", inputList);
-                        Log.w("LOGIN OUTPUT",output.toString());
-                        if(output.length()>0)
-                            Toast.makeText(getApplicationContext(), output.toString(), Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(getApplicationContext(), "NULL", Toast.LENGTH_LONG).show();
-
-                    }
-                    catch(Exception e)
-                    {
-                        Log.e("OTH Error", "I got an error", e);
-                        Toast.makeText(getApplicationContext(), "Technical Error, contact support", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-
-            signUp.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v){
-                    Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    LoginActivity.this.startActivity(registerIntent);
-                }
-            });
-            skip.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    Intent offerIntent = new Intent(LoginActivity.this, OffersList.class);
-                    LoginActivity.this.startActivity(offerIntent);
-                }
-            });
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
         }
+
+        final EditText email = (EditText) findViewById(R.id.loginEmail);
+        final EditText password = (EditText) findViewById(R.id.loginPassword);
+        final Button loginButton= (Button)findViewById(R.id.loginButton);
+
+        final TextView register = (TextView) findViewById(R.id.register);
+        final TextView skip = (TextView) findViewById(R.id.skip);
+        final TextView reset = (TextView) findViewById(R.id.resetPassword);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String emailStr = email.getText().toString();
+                String passStr = password.getText().toString();
+
+                APIConnection connection = new APIConnection();
+                ArrayList<String> inputList = new ArrayList<String>();
+                inputList.add("email="+emailStr);
+                inputList.add("password="+passStr);
+                try
+                {
+                    StringBuffer output = connection.sendPost("/api/v1/member/login", inputList);
+                    Log.w("LOGIN OUTPUT",output.toString());
+                    if(output.length()>0)
+                        Toast.makeText(getApplicationContext(), output.toString(), Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getApplicationContext(), "NULL", Toast.LENGTH_LONG).show();
+
+                }
+                catch(Exception e)
+                {
+                    Log.e("OTH Error", "I got an error", e);
+                    Toast.makeText(getApplicationContext(), "Technical Error, contact support", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+        skip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent offerIntent = new Intent(LoginActivity.this, OffersList.class);
+                LoginActivity.this.startActivity(offerIntent);
+            }
+        });
+    }
 }
