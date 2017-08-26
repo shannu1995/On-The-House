ed9bb0d4ce38b08c86521f76d49de5c8f0ae66d9
anashanifm <anashanif@outlook.com>
2017-08-27 04:08:02 +1000

    testing

--- app/src/main/java/com/onthehouse/onthehouse/RegisterActivity.java
+++ app/src/main/java/com/onthehouse/onthehouse/RegisterActivity.java
@@ -28,7 +28,6 @@ import cn.xm.weidongjian.progressbuttonlib.ProgressButton;
 
 public class RegisterActivity extends AppCompatActivity
 {
-
     EditText regEmail;
     EditText regPass;
     EditText regCPass;
