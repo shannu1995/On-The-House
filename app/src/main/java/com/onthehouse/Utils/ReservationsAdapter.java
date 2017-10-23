d6db0192d4f400b5481beef539d601a5ff890a3e
Abu Bakar Khan <s3597217@student.rmit.edu.au>
2017-10-23 15:43:21 +1100

    Past offerings done

--- app/src/main/java/com/onthehouse/Utils/ReservationsAdapter.java
+++ app/src/main/java/com/onthehouse/Utils/ReservationsAdapter.java
@@ -26,6 +26,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapter.MyViewHolder> {
 
     private Context context;
@@ -82,7 +84,7 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
             alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Rate",
                     new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            ratingDialog(context);
+                            ratingDialog(context, reservation);
                         }
                     });
         }
@@ -103,7 +105,7 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
         alertDialog.show();
     }
 
-    public void ratingDialog(final Context context) {
+    public void ratingDialog(final Context context, final Reservation reservation) {
         final AlertDialog alertDialog = new AlertDialog.Builder(context)
                 .setPositiveButton("Submit", null)
                 .create();
@@ -113,10 +115,11 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
         alertDialog.setView(ratingView);
 
         final RatingBar ratingBar = ratingView.findViewById(R.id.rb_rate_event);
+        final TextView comments = ratingView.findViewById(R.id.et_provide_comments);
 
         alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
-            public void onShow(DialogInterface dialog) {
+            public void onShow(final DialogInterface dialog) {
                 Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -125,7 +128,18 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
                             Toast.makeText(context, "Please provide a rating", Toast.LENGTH_LONG).show();
                             alertDialog.show();
                         } else {
-                            Toast.makeText(context, "To be implemented", Toast.LENGTH_LONG).show();
+                            ArrayList<String> ratingInput = new ArrayList<>();
+                            ratingInput.add("&event_id=" + reservation.getEvent_id());
+                            ratingInput.add("&member_id=" + Member.getInstance().getId());
+                            ratingInput.add("&rating=" + (int) ratingBar.getRating());
+                            if (!comments.getText().toString().trim().equalsIgnoreCase("")) {
+                                ratingInput.add("&comments=" + comments.getText().toString());
+                            }
+                            new rateEventAsyncData(context).execute(ratingInput);
+                            dialog.dismiss();
+                            reservation.setCan_rate(false);
+                            reservation.setHas_rated(true);
+                            notifyDataSetChanged();
                         }
                     }
                 });
@@ -183,6 +197,55 @@ public class ReservationsAdapter extends RecyclerView.Adapter<ReservationsAdapte
             event_venue = itemView.findViewById(R.id.tv_event_venue);
             rate_event = itemView.findViewById(R.id.tv_rate_event);
             layout = itemView.findViewById(R.id.cl_reservations);
+        }
+    }
+
+    public class rateEventAsyncData extends AsyncTask<ArrayList<String>, Void, Integer> {
+        Context context;
+        String errorText;
+        ProgressDialog progressDialog;
+
+        public rateEventAsyncData(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Integer doInBackground(ArrayList<String>... params) {
+            int status;
+            APIConnection connection = new APIConnection();
+            try {
+                String output = connection.sendPost("/api/v1/event/rate", params[0]);
+                if (output.length() > 0) {
+                    JSONObject obj = new JSONObject(output);
+                    String result = obj.getString("status");
+                    if (result.equals("success")) {
+                        status = 1;
+                    } else {
+                        status = 2;
+                        JSONObject jsonArray = obj.getJSONObject("error");
+                        JSONArray errorArr = jsonArray.getJSONArray("messages");
+                        errorText = errorArr.getString(0);
+                    }
+                } else {
+                    status = 3;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "doInBackground: " + e.getMessage());
+                status = 3;
+            }
+            return status;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if (result == 1) {
+                Toast.makeText(context, "Rating submitted, thank you for your feedback", Toast.LENGTH_LONG).show();
+            } else if (result == 2) {
+                Toast.makeText(context, errorText, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Submission failed, technical error.", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
