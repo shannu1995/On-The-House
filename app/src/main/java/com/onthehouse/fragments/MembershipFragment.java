84ec1e2e39e7ff653c73e79f995a7a599170a53a
Abu Bakar Khan <s3597217@student.rmit.edu.au>
2017-10-25 03:00:26 +1100

    Membership changes (In progress)
    Fixed fragment back stack
    
    Update theme (Few Design Changes)

--- app/src/main/java/com/onthehouse/fragments/MembershipFragment.java
+++ app/src/main/java/com/onthehouse/fragments/MembershipFragment.java
@@ -11,21 +11,26 @@ import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.onthehouse.Utils.DrawerLocker;
 import com.onthehouse.connection.APIConnection;
 import com.onthehouse.details.Member;
+import com.onthehouse.details.Membership;
 import com.onthehouse.onthehouse.R;
 
-import java.util.ArrayList;
+import org.json.JSONObject;
 
-import static android.content.ContentValues.TAG;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MembershipFragment extends Fragment {
 
+    private static final String TAG = "MembershipFragment";
     private RadioGroup membershipLevel;
     private RadioButton gold_radio, bronze_radio;
+    private Membership currentMembership;
+    private ArrayList<Membership> membershipArrayList;
 
     public MembershipFragment() {
         // Required empty public constructor
@@ -37,9 +42,11 @@ public class MembershipFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_member, container, false);
         final Context mContext = container.getContext();
 
+        membershipArrayList = new ArrayList<>();
         membershipLevel = view.findViewById(R.id.rg_memberships);
         gold_radio = view.findViewById(R.id.radioButton_gold);
         bronze_radio = view.findViewById(R.id.radioButton_bronze);
+        currentMembership = new Membership();
 
         ArrayList<String> inputList = new ArrayList<>();
         inputList.add("&member_id=" + Member.getInstance().getId());
@@ -48,7 +55,33 @@ public class MembershipFragment extends Fragment {
         return view;
     }
 
+    public class pastMembershipAsynData extends AsyncTask<ArrayList<String>, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            //Lock Drawer While Loading
+            ((DrawerLocker) getActivity()).setDrawerEnabled(false);
+        }
+
+        @Override
+        protected Integer doInBackground(ArrayList<String>... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            //Unlock Drawer While Loading
+            ((DrawerLocker) getActivity()).setDrawerEnabled(true);
+        }
+    }
+
     public class currentMembershipAsyncData extends AsyncTask<ArrayList<String>, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            //Lock Drawer While Loading
+            ((DrawerLocker) getActivity()).setDrawerEnabled(false);
+        }
 
         @Override
         protected Integer doInBackground(ArrayList<String>... params) {
@@ -56,9 +89,35 @@ public class MembershipFragment extends Fragment {
             int status = 0;
             APIConnection connection = new APIConnection();
             try {
-                String output = connection.sendPost("/api/v1/member/membership/history", params[0]);
+                String output = connection.sendPost("/api/v1/member/membership", params[0]);
                 if (output.length() > 0) {
-                    System.out.println(output);
+                    JSONObject obj = new JSONObject(output);
+                    String result = obj.getString("status");
+
+                    if (result.equals("success")) {
+                        try {
+                            JSONObject jsonArray = obj.getJSONObject("membership");
+                            currentMembership.setId(jsonArray.getInt("id"));
+                            currentMembership.setMembership_level_id(jsonArray.getInt("membership_level_id"));
+                            currentMembership.setMember_id(jsonArray.getInt("member_id"));
+                            currentMembership.setDate_expires(jsonArray.getLong("date_expires"));
+                            currentMembership.setPrice(jsonArray.getDouble("price"));
+                            currentMembership.setPaypayl_data(jsonArray.getString("paypal_data"));
+                            currentMembership.setMembership_level_name(jsonArray.getString("membership_level_name"));
+                            currentMembership.setDate_created(jsonArray.getLong("date_created"));
+
+                            status = 1;
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.w("Error", e.getMessage());
+                            status = 3;
+                        }
+                    } else {
+                        status = 2;
+                        //Wrong Details
+                        Log.d(TAG, "doInBackground: Wrong details, Status = " + status);
+                    }
                 }
 
             } catch (Exception e) {
@@ -67,6 +126,26 @@ public class MembershipFragment extends Fragment {
                 status = 3;
             }
             return status;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            System.out.println(currentMembership.toString());
+            try {
+                if (currentMembership.getMembership_level_name().equalsIgnoreCase("gold")) {
+                    gold_radio.setChecked(true);
+                } else {
+                    bronze_radio.setChecked(true);
+                }
+            } catch (NullPointerException e) {
+                //API returns null if no membership changes have been made ever
+                bronze_radio.setChecked(true);
+                //Set Manually for comparison
+                currentMembership.setMembership_level_name("Bronze");
+                currentMembership.setMembership_level_id(3);
+            }
+            //Unlock Drawer While Loading
+            ((DrawerLocker) getActivity()).setDrawerEnabled(true);
         }
     }
 }
--- app/src/main/java/com/onthehouse/fragments/OffersList.java
+++ app/src/main/java/com/onthehouse/fragments/OffersList.java
@@ -2,8 +2,6 @@ package com.onthehouse.fragments;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,7 +10,6 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +21,7 @@ import com.onthehouse.connection.APIConnection;
 import com.onthehouse.details.OfferDetail;
 import com.onthehouse.details.Offers;
 import com.onthehouse.details.UtilMethods;
+import com.onthehouse.onthehouse.MainMenu;
 import com.onthehouse.onthehouse.R;
 
 import org.json.JSONArray;
@@ -54,8 +52,8 @@ public class OffersList extends Fragment
         Context mContext = container.getContext();
 
         offerDetails = OfferDetail.getInstance();
-        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        mSwipeRefreshLayout = view.findViewById(R.id.swipe_container);
 
         offersList = new ArrayList<>();
         adapter = new OffersAdapter(mContext, offersList);
@@ -74,7 +72,7 @@ public class OffersList extends Fragment
             }
         });
 
-        ArrayList<String> inputList = new ArrayList<String>();
+        ArrayList<String> inputList = new ArrayList<>();
         inputList.clear();
 
         new inputAsyncData(mContext).execute(inputList);
@@ -84,16 +82,12 @@ public class OffersList extends Fragment
 
     void refreshItems()
     {
-
         offersList = new ArrayList<>();
 
-        ArrayList<String> inputList = new ArrayList<String>();
+        ArrayList<String> inputList = new ArrayList<>();
         inputList.clear();
 
         new inputAsyncData(getActivity()).execute(inputList);
-        //Intent offerIntent = new Intent(getActivity(), MainMenu.class);
-        //startActivity(offerIntent);
-
         onItemsLoadComplete();
     }
 
@@ -103,52 +97,6 @@ public class OffersList extends Fragment
 
         // Stop refresh animation
         mSwipeRefreshLayout.setRefreshing(false);
-    }
-
-    /**
-     * RecyclerView item decoration - give equal margin around grid item
-     */
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
-    }
-
-    /**
-     * Converting dp to pixel
-     */
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
     public class inputAsyncData extends AsyncTask<ArrayList<String>, Void, Integer>
@@ -162,17 +110,19 @@ public class OffersList extends Fragment
 
         protected void onPreExecute()
         {
-            // Create a progressdialog
+            // Create a progress dialog
             mProgressDialog = new ProgressDialog(getActivity());
-            // Set progressdialog title
+            // Set progress dialog title
             mProgressDialog.setTitle("Getting offers....");
-            // Set progressdialog message
+            // Set progress dialog message
             mProgressDialog.setMessage("Loading...");
             mProgressDialog.setIndeterminate(false);
-            // Show progressdialog
+            // Show progress dialog
             mProgressDialog.show();
             //Lock Drawer While Loading
-            ((DrawerLocker) getActivity()).setDrawerEnabled(false);
+            if (getActivity() instanceof MainMenu) {
+                ((DrawerLocker) getActivity()).setDrawerEnabled(false);
+            }
         }
 
         @Override
@@ -291,8 +241,6 @@ public class OffersList extends Fragment
                                 //detail.setDates(showDates);
                                 offerDetails.add(detail);
                             }
-                            //Log.w("eventtt", offerDetails.get(0).getDescription());
-                            //Log.w("eventtt", offerDetails.get(1).getDescription());
 
                             status = 1;
                         }
@@ -301,9 +249,6 @@ public class OffersList extends Fragment
                             Log.w("Event error", e.getMessage());
                             status = 3;
                         }
-
-                        //Log.w("status", String.valueOf(status));
-                        //Log.w("OBJECT TEST", member.getFirst_name());
                     }
 
                     else
@@ -355,8 +300,10 @@ public class OffersList extends Fragment
             }
             adapter.notifyDataSetChanged();
             mProgressDialog.dismiss();
-            //Unlock Drawer
-            ((DrawerLocker) getActivity()).setDrawerEnabled(true);
+            if (getActivity() instanceof MainMenu) {
+                //Unlock Drawer
+                ((DrawerLocker) getActivity()).setDrawerEnabled(true);
+            }
         }
     }
 }
